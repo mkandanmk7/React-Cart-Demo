e71a16adc0689fd3b725604a4f3201abe6536a93
mkandanmk7 <mkandanmk7@gmail.com>
2021-12-17 10:10:37 +0530

    cart Details

--- src/Components/CartDetails.jsx
+++ src/Components/CartDetails.jsx
@@ -9,28 +9,37 @@ const CartDetails = () => {
   console.log("added products to cart:", cart);
   return (
     <div className="c_container">
-      <div className="c_innercontainer">
-        <img src="h" alt="product_image" />
-        <div className="c_details">
-          <div className="c_price_name">
-            <h3 className="c_name">name</h3>
-            <div className="c_price">price</div>
+      {cart.map((product) => {
+        return (
+          <div key={product.id} className="c_innercontainer">
+            <img src={product.image} className="c_image" alt="product_image" />
+            <div className="c_details">
+              <div className="c_price_name">
+                <h3 className="c_name">{product.name}</h3>
+                <div className="c_price">{product.price}</div>
+              </div>
+              <div className="inc">
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  className="inc_quantity"
+                >
+                  +
+                </Button>
+                <p className="value">5</p>
+                <Button variant="contained" color="primary">
+                  -
+                </Button>
+              </div>
+              <div className="remove">
+                <Button variant="outlined" color="prima">
+                  Delete
+                </Button>
+              </div>
+            </div>
           </div>
-          <div className="inc">
-            <Button
-              variant="contained"
-              color="secondary"
-              className="inc_quantity"
-            >
-              +
-            </Button>
-            <p className="value">5</p>
-            <Button variant="contained" color="primary">
-              -
-            </Button>
-          </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 };
